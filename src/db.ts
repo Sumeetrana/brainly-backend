49733ff38cad95e66f8c07ec4ffc89f85e1dffce
Sumeet Rana <sumeetsinh28@gmail.com>
2025-07-26 17:16:51 +0400

    feat: signin and signup schema with jwt

---
+++ src/db.ts
@@ -0,0 +1,22 @@
+import { model, Schema, connect, Types } from "mongoose";
+
+connect(
+  "mongodb+srv://root:root@cluster0.xrvnj87.mongodb.net/brainly?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  tags: [{ type: Types.ObjectId, ref: "Tag" }],
+  userId: { type: Types.ObjectId, ref: "User", required: true },
+});
+
+const UserModel = model("users", UserSchema);
+const ContentModel = model("content", ContentSchema);
+
+export { UserModel };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,62 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+
+import { UserModel } from "./db";
+
+const JWT_SECRET = "S3cret";
 
 const app = express();
 
-app.post("/api/v1/signup", (req, res) => {});
+app.use(express.json());
 
-app.post("/api/v1/signin", (req, res) => {});
+app.post("/api/v1/signup", async (req, res) => {
+  // TODO: hash password, zod validation, Handle different scenarios
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.get("/api/v1/content", (req, res) => {});
+  try {
+    await UserModel.create({
+      username,
+      password,
+    });
+
+    res.json({
+      message: "User signed up",
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  const exisitingUser = await UserModel.findOne({
+    username,
+  });
+
+  if (exisitingUser) {
+    const token = jwt.sign(
+      {
+        id: exisitingUser._id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials!",
+    });
+  }
+});
+
+app.get("/api/v1/content", (req, res) => {
+  const { type, link } = req.body;
+});
 
 app.post("/api/v1/content", (req, res) => {});
 
